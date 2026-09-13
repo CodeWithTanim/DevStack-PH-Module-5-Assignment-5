9628014d0c0facf0cffa13fd4ec0776ecec97050
MD. SAMIUR RAHMAN TANIM <codewithtanim+support@gmail.com>
2026-09-13 15:15:07 +0530

    Navbar Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1>Hello Bangladesh</h1>
+      <Navbar/>
+      {/* <h1>Hello Bangladesh</h1> */}
     </>
   )
 }
